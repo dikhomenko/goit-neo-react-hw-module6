334458e1454d7191bab04e78bbf84047a68a235d
dkhomenko <dinamastercs@gmail.com>
2025-01-08 15:20:43 +0100

    edit items in initialState

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,10 +4,8 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Britney Spears', number: '555-555-5555' },
+      { id: 'id-2', name: 'Bill Clinton', number: '555-666-7777' },
     ],
   },
   reducers: {
