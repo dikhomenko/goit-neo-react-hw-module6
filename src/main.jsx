a404b63528af45372e4ad32b5ec17ab3aff91856
dkhomenko <dinamastercs@gmail.com>
2025-01-08 15:07:53 +0100

    add redux to Contact Book

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import App from './components/App/App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
